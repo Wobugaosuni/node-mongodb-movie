f444ec86af67f59fc3b1a9165ee5489451588d24
qiuxia <397401374@qq.com>
2018-11-17 16:34:44 +0800

    category schema

--- app/models/db.js
+++ app/models/db.js
@@ -3,16 +3,19 @@ var mongoose = require('mongoose');
 var MoiveSchema = require('../schemas/movies');
 var UserSchema = require('../schemas/user')
 var CommentSchema = require('../schemas/comment')
+var CategorySchema = require('../schemas/category')
 
 // 模型
 var Movie = mongoose.model('movies_collection', MoiveSchema);
 var User = mongoose.model('user_collection', UserSchema);
 var Comment = mongoose.model('comment_collection', CommentSchema);
+var Category = mongoose.model('category_collection', CategorySchema);
 
 const db = {
   Movie,
   User,
   Comment,
+  Category,
 }
 
 module.exports = db;
---
+++ app/schemas/category.js
@@ -0,0 +1,59 @@
+/**
+ * 电影的分类
+ * 一个类别下有多部电影
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ObjectId = Schema.Types.ObjectId
+
+// 定义模式
+var CategorySchema = new Schema({
+	name: String,
+  movies: [{  // 通过 ref 引用查找对应的数据：ObjectId
+    type: ObjectId,
+    ref: 'movies-collection',
+  }],
+	meta: {
+		createAt: {
+			type: Date,
+			default: Date.now()
+		},
+		updateAt: {
+			type: Date,
+			default: Date.now()
+		}
+	}
+});
+
+
+// 触发 save 之前的钩子，更新时间
+CategorySchema.pre('save', function (next) {
+	if (this.isNew) {
+		this.meta.createAt = this.meta.updateAt = Date.now();
+	} else {
+		this.meta.updateAt = Date.now();
+	}
+	next()
+});
+
+
+// 定义静态方法
+// 返回全部数据
+CategorySchema.static('fetch', function (cb) {
+	return this
+		.find()
+		.sort('meta.updateAt') // 升序
+		.exec(cb);
+})
+
+// 返回一条数据
+CategorySchema.static('findById', function (id, cb) {
+	return this
+		.findOne({
+			_id: id
+		})
+		.exec(cb);
+})
+
+module.exports = CategorySchema;
